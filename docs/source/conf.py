51bceccd5c33cd0f2fc148c27304539c9bc73f33
hoonhee92 <147481170+hoonhee92@users.noreply.github.com>
2024-12-08 16:16:43 +0900

    Update conf.py
    제목 수정

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'coremaxtech.opemstack.guide'
+copyright = '2024, coremaxtech'
+author = '박훈희'
 
 release = '0.1'
 version = '0.1.0'
